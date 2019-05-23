b76cc584bc9877d1b63608ac7c181c7d671ebe29
Hayliiel <rodrigo.oi@hotmail.com>
2019-05-23 03:32:25 -0300

    Fixing some things(There are still errors)

--- Searches/Binary_Search/binary_iterative.py
+++ Searches/Binary_Search/binary_iterative.py
@@ -3,46 +3,34 @@
 """
 
  This function is the basic binary search, it receives an element
- and a list where it will search for the desired element.
+ and a list_1 where it will search for the desired element.
  The function returns if the element was found or not and
  the position of it (if it was found)
 
  TODO: Tests
 """
 
-def binary_iterative(element, list):
+def binary_iterative(element, list_1):
 
-	list.sort() # It sorts the list since binary search only works on ordered lists
-	mid = len(list) / 2
-	
-	if element == list[mid]: # Checks if the desired element is in the middle of the list
+	list_1.sort() # It sorts the list_1 since binary search only works on ordered lists
+	print(list_1)
+	lower = 0
+	upper = len(list_1)
 
-		print("Element found in position"),
-		print(list.index(mid))
-		return
+	while lower <= upper:
 
-	elif element > mid:
+		mid = int((upper - lower) / 2)
 
-		start = mid
+		if element == list_1[mid]: # Checks if the element is on the middle of the list
+			print("Element found in position", mid)
+			return
+		
+		elif element < list_1[mid]: # Checks if the element is lower than the middle of the list
+			upper = mid - 1
 
-		while element > mid: # Loop for searching the element on the upper part of the list
-			mid = int((mid + len(list)) / 2)
+		else: # If the element isn't lower or equal to the middle, its higher. (Or is not on the list)
+			lower = mid + 1
 
-			if element == list[mid]:
-				print("Element found in position"),
-				print(list.index(mid))
-				return
-	elif element < mid:
+	print("Element not found")
+	return
 
-		end = mid
-
-		while element < mid: # Loop for searching the element on the lower part of the list
-			mid = int(mid / 2)
-
-			if element == list[mid]:
-				print("Element found in position"),
-				print(list.index(mid))
-				return
-	else:
-		print("Element not found")
-		return
